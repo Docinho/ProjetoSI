25fe87575cd919b59a32eb53b61946c6f9d51aa2
marcusvlc <marcus.costa@ccc.ufcg.edu.br>
2018-02-28 10:43:40 -0300

    Corrigindo erro de produto nao fica disponivel ao adicionar lote

--- src/main/webapp/app/core/filters/disponivel.js
+++ src/main/webapp/app/core/filters/disponivel.js
@@ -1,6 +1,6 @@
 app.filter("disponivel", function() {
     return function(situation) {
-        if (situation) {
+        if (situation == 1) {
             return "Disponível"
         } else {
             return "Em falta"
